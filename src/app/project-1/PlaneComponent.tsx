01b703b593e532075b5ad3cbbbccee3d6f1160ef
paulrvach <paulrvach@gmail.com>
2023-09-25 13:34:20 -0700

    deleted all files for rehaul merge

--- src/app/project-1/PlaneComponent.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/3nDK2UNtlVaQyyfK/scene.splinecode" />
-  );
-}
-
