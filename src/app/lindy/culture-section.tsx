885dbaa1d611a630cdfb260839b702751d7080f9
paulrvach <paulrvach@gmail.com>
2023-10-03 15:42:26 -0700

    mission

--- src/app/lindy/culture-section.tsx
+++ src/app/lindy/culture-section.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "@radix-ui/themes";
 import { values } from "./lindy-values";
 import CultureValue from "./culture-value";
 type Props = {};
@@ -6,6 +7,7 @@ type Props = {};
 const CultureSection = (props: Props) => {
   return (
     <div className="w-full h-full">
+      <Text size={"6"} mb={"2"}>{"Where I stand on Lindy's values:"}</Text>
         <CultureValue
          values={values}
         />
